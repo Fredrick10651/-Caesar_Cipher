cdd11b7ccc95e492cf7d0d6e144ac991a948eca1
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-08-01 09:17:31 +0300

    Add refracted codes for the caesaer cipher functioning

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
 
 public class CaesarCipher {
+private StringBuffer mOutcome = new StringBuffer();
+private String mOutcomeString = new String();
+
+    public StringBuffer getmOutcome() {
+        return mOutcome;
+    }
+
+    public String getmOutcomeString() {
+        return mOutcomeString;
+    }
+
+    public String doCaesarCipher(String phraseForCaeserCipher, int shiftIndex){
+        for (int i=0; i<phraseForCaeserCipher.length(); i++){
+            if (Character.isUpperCase(phraseForCaeserCipher.charAt(i))){
+              char charr = (char)(((int)phraseForCaeserCipher.charAt(i) + shiftIndex - 65)% 26 + 65);
+              mOutcome.append(charr);
+              mOutcomeString = mOutcome.toString();
+            }
+            else{
+                char charr = (char)(((int)phraseForCaeserCipher.charAt(i) + shiftIndex -97)% 26 + 97);
+                mOutcome.append(charr);
+                mOutcomeString = mOutcome.toString();
+            }
+        }
+        return mOutcomeString;
+    }
+    public String unDoCaeserCipher(String phraseForCaeserCipher, int shiftIndex){
+        return doCaesarCipher(phraseForCaeserCipher, 26 - shiftIndex);
+    }
 }
