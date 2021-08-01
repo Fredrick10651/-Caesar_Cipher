3bf617efa700c46226d3bbbadf41eddab73a93ce
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-08-01 09:19:44 +0300

    Add uppercase,lowercase,encryption and decreption codes for the tests

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -1,5 +1,53 @@
+import org.junit.Test;
+
+
+
 import static org.junit.Assert.*;
 
 public class CaesarCipherTest {
 
-}
+    @Test
+    public void CaeserCipher_instanceOf_Object() {
+        CaesarCipher testCaeserCipher = new CaesarCipher();
+                assertTrue(testCaeserCipher instanceof CaesarCipher);
+    }
+
+    @Test
+    public void CaeserCipher_takeStringInput_string(){
+        CaesarCipher testCaeserCipher = new CaesarCipher();
+        String expectedOutput = new String();
+        expectedOutput = "rcuuyqtf";
+        assertEquals(expectedOutput, testCaeserCipher.doCaesarCipher("password", 2));
+    }
+
+    @Test
+    public void CaeserCipher_enableUpperCaseInput_string(){
+        CaesarCipher testCaeserCipher = new CaesarCipher();
+        String expectedOutput = new String();
+        expectedOutput = "RCUUYQTF";
+        assertEquals(expectedOutput, testCaeserCipher.doCaesarCipher("PASSWORD", 2));
+    }
+
+    @Test
+    public void CaeserCipher_doEncryption_string(){
+        CaesarCipher testCaeserCipher =new CaesarCipher();
+        String expectedOutput = new String();
+        expectedOutput = "rcuuyqtf";
+        assertEquals(expectedOutput, testCaeserCipher.doCaesarCipher("password", 2));
+    }
+
+    @Test
+    public void CaeserCipher_unDoDecryption_string(){
+        CaesarCipher testCaeserCipher = new CaesarCipher();
+        String expectedOutput = new String();
+        expectedOutput = "password";
+        assertEquals(expectedOutput, testCaeserCipher.unDoCaeserCipher( "rcuuyqtf", 2));
+    }
+
+    @Test
+    public void unDoCaeserCipher_insertsUpperCaseLetters_string(){
+        CaesarCipher testCaeserCipher = new CaesarCipher();
+        String expectedOutput ="HEX";
+        assertEquals(expectedOutput, testCaeserCipher.unDoCaeserCipher("EBU", 3));
+    }
+    }
