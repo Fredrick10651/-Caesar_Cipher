85bf2741cf061b997a136c4cd0b3ceb26bf860e5
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-08-02 00:17:39 +0300

    Add decryption test and codes in the project

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -16,38 +16,32 @@ public class CaesarCipherTest {
     public void CaeserCipher_takeStringInput_string(){
         CaesarCipher testCaeserCipher = new CaesarCipher();
         String expectedOutput = new String();
-        expectedOutput = "rcuuyqtf";
-        assertEquals(expectedOutput, testCaeserCipher.doCaesarCipher("password", 2));
+        expectedOutput = "tomorrow";
+        assertEquals(expectedOutput, testCaeserCipher.doCaesarCipher("pkiknnks", 4));
     }
 
-    @Test
-    public void CaeserCipher_enableUpperCaseInput_string(){
-        CaesarCipher testCaeserCipher = new CaesarCipher();
-        String expectedOutput = new String();
-        expectedOutput = "RCUUYQTF";
-        assertEquals(expectedOutput, testCaeserCipher.doCaesarCipher("PASSWORD", 2));
-    }
 
     @Test
     public void CaeserCipher_doEncryption_string(){
         CaesarCipher testCaeserCipher =new CaesarCipher();
         String expectedOutput = new String();
-        expectedOutput = "rcuuyqtf";
-        assertEquals(expectedOutput, testCaeserCipher.doCaesarCipher("password", 2));
+        expectedOutput = "tomorrow";
+        assertEquals(expectedOutput, testCaeserCipher.doCaesarCipher("pkiknnks", 4));
     }
 
     @Test
     public void CaeserCipher_unDoDecryption_string(){
         CaesarCipher testCaeserCipher = new CaesarCipher();
         String expectedOutput = new String();
-        expectedOutput = "password";
-        assertEquals(expectedOutput, testCaeserCipher.unDoCaeserCipher( "rcuuyqtf", 2));
+        expectedOutput = "pkiknnks";
+        assertEquals(expectedOutput, testCaeserCipher.unDoCaeserCipher( "tomorrow", 4));
     }
 
-    @Test
-    public void unDoCaeserCipher_insertsUpperCaseLetters_string(){
+       @Test
+    public void CaeserCipher_enableUpperCaseInput(){
         CaesarCipher testCaeserCipher = new CaesarCipher();
-        String expectedOutput ="HEX";
-        assertEquals(expectedOutput, testCaeserCipher.unDoCaeserCipher("EBU", 3));
-    }
+         String expectedOutput = new String();
+       expectedOutput = "TOMORROW";
+        assertEquals(expectedOutput, testCaeserCipher.doCaesarCipher("PKIKNNKS", 4) );
+       }
     }
