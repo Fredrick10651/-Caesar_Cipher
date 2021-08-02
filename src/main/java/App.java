ef7988f26daaa958b5c7501eb99200784772a904
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-08-02 09:50:41 +0300

    Remove comments from App.java codes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,28 +11,28 @@ public class App {
         String userInput = myScanner.nextLine();
 
         if (userInput.equals("encryption")){
-            System.out.println("                          ");
+            System.out.println("");
             System.out.println("type phrase for encryption");
             String phraseToEncrypt = myScanner.nextLine();
-            System.out.println("                     ");
+            System.out.println("");
             System.out.println("enter the number for index to be encrypted");
             int shiftIndexEncrypt = Integer.parseInt(myScanner.nextLine());
-            System.out.println("                             ");
+            System.out.println("");
             System.out.println(myCaesarCipher.doCaesarCipher(phraseToEncrypt, shiftIndexEncrypt));
         }
         else if (userInput.equals("decryption")){
-            System.out.println("                       ");
+            System.out.println("");
             System.out.println("enter phrase for decryption");
             String phraseToDecrypt = myScanner.nextLine();
-            System.out.println("                           ");
+            System.out.println("");
             System.out.println("enter number for index to be decrypted");
             int shiftIndexDecrypt = Integer.parseInt(myScanner.nextLine());
-            System.out.println("                                  ");
+            System.out.println("");
             System.out.println(myCaesarCipher.unDoCaeserCipher(phraseToDecrypt, shiftIndexDecrypt));
 
         }
         else {
-            System.out.println("                               ");
+            System.out.println("");
             System.out.println("Error!");
         }
     }
